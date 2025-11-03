cee43b6540ad0ff034f7cc6250f98c73dfcb1d69
Uttam <uttamrohit4545@gmail.com>
2025-11-04 02:05:51 +0600

    feat : added user schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const mongoose = require('mongoose');
+require('dotenv').config()
+const cors = require('cors')
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+//mongoose connect with mongodb
+
+const uri = process.env.DB_URL
+async function main() {
+  await mongoose.connect(uri);
+}
+
+main().then(() => console.log("Mongodb connected successfully")).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ backend/src/users/user.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+
+    username:{type: String , required:true,unique: true},
+    email: {type: String , required:true, unique: true},
+    password: {type:String , required:true},
+    profileImage: String,
+    Bio: {type:String , maxLength : 200},
+    profession: String,
+    role:{
+        type:String,
+        default : 'user'
+    },
+    createdAt : {type:Date , default:Date.now}
+})
+
+
+const User = model('User' , userSchema);
+
+module.exports = User
