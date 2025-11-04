e7f84534b4b1bf919e4be980f7fd5dbcef422a05
Uttam <uttamrohit4545@gmail.com>
2025-11-04 14:50:03 +0600

    feat : user login route

--- backend/src/users/user.routes.js
+++ backend/src/users/user.routes.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const { userRegistration } = require('./user.controller');
+const User = require('./user.model');
 const router = express.Router()
 
 
 //register
 router.post('/register',userRegistration);
+
 //login
-router.login('/login' , async(req,res) => {
+router.post('/login' , async(req,res) => {
+    const {email , password} = req.body
+
+    try{
+        const user = await  User.findOne({email})
+
+        if(!user || !(await user.isPasswordMatch(password))){
+            return res.status(401).send({
+                message : "Incorrect email or password"
+            })
+        }
+    }catch(error){
+        res.send({
+            error
+        })
+    }
     
 })
 
