a0cd30a691220ad4750e1c2b25f773ac16f633a9
Uttam <uttamrohit4545@gmail.com>
2025-11-27 20:20:00 +0600

    FEAT:config url

--- backend/src/orders/order.controller.js
+++ backend/src/orders/order.controller.js
@@ -6,8 +6,23 @@ const PaymentRequest = async(req,res) => {
     const {products,userId} = req.body
 
     try{
+        const line_item = products.map((product) => ({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: product.name,
+                    images: [product.image]
+                },
+                unit_amount:Math.round(product.price * 100),
+
+            }
+        }))
 
         const session = await stripe.checkout.sessions.create({
+            line_item : line_item,
+            payment_method_types : ["card"],
+            mode :"payment",
+            
 
         })
            
